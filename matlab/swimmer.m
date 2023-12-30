0538b6cbe08788f31bde250e36a39a4075816706
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-12-29 22:47:59 -0600

    Revising the swimmer.m script.

--- matlab/swimmer.m
+++ matlab/swimmer.m
@@ -1,25 +1,44 @@
+% TODO: Explain here what this script is for, and how to use it.
+
+% Initialize the pseudorandom number sequence.
 rng(1);
-% k = 17; % Number of factors.
-% sW = 0.95; % Average sparsity of W.
-sW = 0;
-k = 16;
+
+% Load the swimmer data set.
 load('../data/swimmer.mat');
-Y = reshape(Y,1024,256);
+n = 1024;
+m = 256;
+Y = reshape(Y,n,m);
 Y = Y - 1;
-options.sW = sW; 
+
+% First run "vanilla" NMF without any sparsity penalty.
+k = 16;
+options.sW = 0;
 options.sH = 0;
 options.maxiter = 100; 
 options.delta = 1e-8;
 [W,H,e,t] = sparseNMF(Y,k,options);
+
+% Compute the sparsity of each column of W.
+sp = zeros(1,k);
+for i = 1:k
+  sp(i) = sp_col(W(:,i));
+end
+save('swimmer_nmf.mat','W','H','sp');
+
+% k = 17; % Number of factors.
+% sW = 0.95; % Average sparsity of W.
+
+return
+
 disp(sum(sum(W > 1e-6))); % Sparsity of W.
 fprintf('Sparsity in columns of H:\n');
 fprintf('%d ',sum(H > 0.001));
 fprintf('\n');
 clf;
-x = zeros(1,k);
+sp = zeros(1,k);
 for i = 1:k
   subplot(5,4,i);
   imshow(reshape(W(:,i),32,32));
-  x(i) = sp_col(W(:,i));
+  sp(i) = sp_col(W(:,i));
 end
-fprintf('Hoyer sparsity of columns of W: %0.3f\n',mean(x));
+fprintf('Hoyer sparsity of columns of W: %0.3f\n',mean(sp));
