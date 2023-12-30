0ccc160873b64249f7480ee7470fc0e185bca503
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-12-30 10:07:52 -0600

    Made some improvements to the plot_images function in swimmer_functions.R.

--- code/swimmer_functions.R
+++ code/swimmer_functions.R
@@ -1,23 +1,22 @@
 # TO DO: Explain here what this function is for, and how to use it.
-plot_images <- function (W) {
-  W <- max(W) - W
-  colnames(W) <- paste0("V", 1:ncol(W))
-  tib <- as_tibble(W)
-  tib <- mutate(tib,
-    row = rep(1:32,each = 32),
-    col = rep(1:32,times = 32)) 
-  tib <- pivot_longer(tib,
-      cols = -c(row,col),
-      names_to = "k",
-      values_to = "loading",
-      names_prefix = "V",
-      names_transform = as.numeric)
-  p <- ggplot(tib,aes(x = row,y = col,fill = loading)) +
+plot_images <- function (W, n = 32, font_size = 9, nrow = 4, ncol = 4) {
+  k <- ncol(W)
+  colnames(W) <- paste0("V",1:k)
+  dat <- as_tibble(W)
+  dat <- mutate(dat,
+                row = rep(1:n,each = n),
+                col = rep(1:n,times = n)) 
+  dat <- pivot_longer(dat,cols = -c(row,col),names_to = "k",
+                      values_to = "loading",names_prefix = "V",
+                      names_transform = as.numeric)
+  dat$k <- factor(dat$k,1:k)
+  return(ggplot(dat,aes(x = row,y = col,fill = loading)) +
     geom_tile() +
     scale_y_reverse() +
     scale_fill_gradient(low = "white",high = "black") +
-    facet_wrap(~k) +
+    facet_wrap(~k,nrow = nrow,ncol = ncol) +
     guides(fill = "none",x = "none",y = "none") +
-    theme_cowplot(font_size = 8)
-  return(p)
+    labs(x = "",y = "") +
+    theme_cowplot(font_size = font_size) +
+    theme(strip.background = element_blank()))
 }
