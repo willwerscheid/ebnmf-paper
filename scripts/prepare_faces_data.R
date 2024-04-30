29ce63d21e9e964f5d53303e7345639fe796ca03
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-04-30 16:52:21 -0500

    Added steps to read data from svm.*.normgrey files.

--- scripts/prepare_faces_data.R
+++ scripts/prepare_faces_data.R
@@ -9,3 +9,28 @@
 # image, histogram equalized and normalized so that all pixel values
 # are between 0 and 1, followed by a 1 for a face or a -1 for a
 # non-face.
+dat <- readLines("svm.train.normgrey")
+
+# Get the number of images.
+n <- as.numeric(dat[[1]])
+
+# Get the number of pixels in each image.
+m <- as.numeric(dat[[2]])
+
+# Initialize the n x m data matrix and the face/non-face labels.
+X <- matrix(0,n,m)
+image_labels <- rep(0,n)
+
+# Extract the images.
+dat <- dat[-(1:2)]
+for (i in 1:n) {
+  pixels <- as.numeric(unlist(strsplit(trimws(dat[[i]])," ")))
+  image_labels[i] <- pixels[m+1]
+  X[i,] <- pixels[1:m]
+}
+
+# Keep only the images that are faces.
+# 1 = face, -1 = non-face.
+i <- which(image_labels == 1)
+image_labels <- image_labels[i]
+X <- X[i,]
