b5ad22065063eb3d1b4095e2ad7ac58c1b6aaf2d
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-05-01 17:52:00 -0500

    Added steps to fit flashier to faces data with fixed and adaptive priors.

--- analysis/faces.R
+++ analysis/faces.R
@@ -28,3 +28,25 @@ nmf <- nnmf(faces_train,k = 49,method = "scd",
             max.iter = 200,rel.tol = 1e-8,
             n.threads = 4,verbose = 2)
 print(plot_faces(nmf$W))
+
+# flashier with fixed prior.
+k <- 49
+fixed_prior <- ebnm_point_exponential(x = rep(1,100))
+fixed_prior$fitted_g$scale <- c(0,1)
+fixed_prior$fitted_g$pi <- c(0.1,0.9)
+ebnm_fixed_prior <- flash_ebnm(prior_family = "point_exponential",
+                               fix_g = TRUE,g_init = fixed_prior)
+fit_fixed <- flash_init(faces_train,var_type = 0)
+fit_fixed <- flash_factors_init(fit_fixed,
+                                list(nmf$W,t(nmf$H)),
+                                ebnm_fixed_prior)
+fit_fixed <- flash_backfit(fit_fixed,verbose = 2)
+print(plot_faces(ldf(fit_fixed,type = "i")$L))
+
+# flashier with adaptive prior.
+fit_adapt <- flash_init(faces_train,var_type = 0)
+fit_adapt <- flash_factors_init(fit_adapt,
+                                list(fit_fixed$L_pm,fit_fixed$F_pm),
+                                ebnm_point_exponential)
+fit_adapt <- flash_backfit(fit_adapt,verbose = 2)
+print(plot_faces(ldf(fit_adaptive,type = "i")$L))
