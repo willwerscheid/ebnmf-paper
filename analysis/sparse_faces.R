9fc8fe0b06ff6cb80e3815251e42de2abc630f5f
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-05-13 16:39:04 -0500

    A few more improvements to sparse_faces.R.

--- analysis/sparse_faces.R
+++ analysis/sparse_faces.R
@@ -34,37 +34,55 @@ set.seed(1)
 k <- 49
 W0 <- fl_baseline$L_pm
 nmf_fixed_baseline <-
-  nnmf(faces_train,k = k,init = list(W0 = W0),method = "scd",max.iter = 4)
+  nnmf(faces_train,k = k,init = list(W0 = W0),method = "scd",max.iter = 4,
+       verbose = 0)
 nmf0 <- nnmf(faces_train,k = k + 1,init = list(W = nmf_fixed_baseline$W),
-             method = "scd",max.iter = 20)
+             method = "scd",max.iter = 20,verbose = 0)
 nmf <- nnmf(faces_train,k = k,method = "scd",max.iter = 200,rel.tol = 1e-8,
-            init = list(W0 = nmf0$W[,k + 1]),
-            n.threads = 4,verbose = 2)
-print(plot_faces(nmf$W,title = "NMF",order_by_sparsity = TRUE))
+            init = list(W0 = nmf0$W[,k + 1]),n.threads = 4,verbose = 2)
 
-# flashier with point-exponential prior.
+# flashier refinement of NMF solution.
+fl_nmf <- flash_init(faces_train,var_type = 0)
+fl_nmf <- flash_factors_init(fl_nmf,
+                             list(nmf$W,t(nmf$H)),
+                             ebnm_point_exponential)
+fl_nmf <- flash_factors_fix(fl_nmf,kset = k + 1,which_dim = "loadings")
+fl_nmf <- flash_backfit(fl_nmf,maxiter = 100,extrapolate = FALSE,verbose = 2)
+fl_nmf <- flash_backfit(fl_nmf,maxiter = 100,extrapolate = TRUE,verbose = 3)
+print(plot_faces(fl_nmf$L_pm,title = "NMF",order_by_sparsity = TRUE))
+
+# flashier initialized to NMF, but with much more wiggle room.
 set.seed(1)
 W0 <- nmf$W[,k + 1]
 nmf0 <- nnmf(faces_train,k = k,init = list(W0 = W0),method = "scd",
              max.iter = 10,rel.tol = 1e-8)
 fl <- flash_init(faces_train,var_type = 0)
-fl <- flash_factors_init(fl,
-                         list(nmf0$W,t(nmf0$H)),
-                         ebnm_point_exponential)
+fl <- flash_factors_init(fl,list(nmf0$W,t(nmf0$H)),ebnm_point_exponential)
 fl <- flash_factors_fix(fl,kset = k + 1,which_dim = "loadings")
 fl <- flash_backfit(fl,maxiter = 100,extrapolate = FALSE,verbose = 2)
 fl <- flash_backfit(fl,maxiter = 100,extrapolate = TRUE,verbose = 3)
 print(plot_faces(fl$L_pm,title = "flashier",order_by_sparsity = TRUE))
 
-# Compare sparsity of NMF and flashier solutions.
-plot(sort(colMeans(normalize.cols(nmf$W) > 0.001)),
-     sort(colMeans(normalize.cols(fl$L_pm) > 0.001)),
-     pch = 20)
-abline(a = 0,b = 1,col = "magenta",lty = "dotted")
+# Compare the ELBOs.
+cat(sprintf("NMF:      %0.4f\n",fl_nmf$elbo))
+cat(sprintf("flashier: %0.4f\n",fl$elbo))
 
-# TO DO NEXT:
-#
-#   + Try NMF with Hoyer sparsity penalty.
-#  
-#   + Assess fit in the test set.
-#
+# Compare sparsity of NMF and flashier-NMF solutions.
+zero <- 0.001
+pdat <-
+  data.frame(nmf      = sort(colMeans(normalize.cols(nmf$W) > zero)),
+             flashier = sort(colMeans(normalize.cols(fl_nmf$L_pm) > zero)))
+p1 <- ggplot(pdat,aes(x = nmf,y = flashier)) +
+  geom_point() +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",lty = "dotted") +
+  theme_cowplot(font_size = 12)
+print(p1)
+
+# Compare sparsity of NMF and flashier solutions.
+pdat <-
+  data.frame(nmf      = sort(colMeans(normalize.cols(nmf$W) > zero)),
+             flashier = sort(colMeans(normalize.cols(fl$L_pm) > zero)))
+p2 <- ggplot(pdat,aes(x = nmf,y = flashier)) +
+  geom_point() +
+  geom_abline(intercept = 0,slope = 1,color = "magenta",lty = "dotted") +
+  theme_cowplot(font_size = 12)
