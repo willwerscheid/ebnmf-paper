409ffdbf9dbc6c73259515b5208969e46b94dc98
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-08-06 12:13:54 -0500

    Added the remaining bird limbs and wrote code to generate all possible birds.

--- analysis/birds.R
+++ analysis/birds.R
@@ -3,21 +3,22 @@ library(cowplot)
 library(tibble)
 library(dplyr)
 library(tidyr)
+source("code/birds_functions.R")
 
 out <- read.csv("data/birds.csv",comment.char = "#",header = FALSE)
 out <- as.matrix(out)
 d   <- 15
 n   <- nrow(out)/d
-birds <- matrix(0,d*d,n)
+limbs <- matrix(0,d*d,n)
 rows <- 1:d
 for (i in 1:n) {
-  birds[,i] <- out[rows,]
+  limbs[,i] <- out[rows,]
   rows <- rows + d
 }
 
 # This takes the m x k matrix of feature weights and plots them as n x
 # n images, where m = n^2. Note that nrow*ncol >= k is expected.
-plot_images <- function (W, n = 15, font_size = 9, nrow = 4, ncol = 4) {
+plot_images <- function (W, n = 15, font_size = 9, nrow = 3, ncol = 3) {
   k <- ncol(W)
   colnames(W) <- paste0("V",1:k)
   dat <- as_tibble(W)
@@ -41,4 +42,26 @@ plot_images <- function (W, n = 15, font_size = 9, nrow = 4, ncol = 4) {
 }
 
 # Plot the features.
-plot_images(birds)
+print(plot_images(limbs))
+
+# Generate all the birds and plot them.
+d     <- sqrt(nrow(limbs))
+dat   <- expand.grid(left_wing  = c(0,3,5),
+                     right_wing = c(0,2,4),
+                     left_foot  = c(0,6,8),
+                     right_foot = c(0,7,9))
+n     <- nrow(dat)
+limbs <- cbind(0,limbs)
+dat   <- dat + 1
+birds <- matrix(0,d*d,n)
+torso <- 2
+for (i in 1:n) {
+  l1 <- dat[i,"left_wing"]
+  l2 <- dat[i,"right_wing"]
+  l3 <- dat[i,"left_foot"]
+  l4 <- dat[i,"right_foot"]
+  birds[,i] <- limbs[,torso] + limbs[,l1] + limbs[,l2] +
+               limbs[,l3] + limbs[,l4]
+}
+    
+print(plot_images(birds,nrow = 7,ncol = 12))
