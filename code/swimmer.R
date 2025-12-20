7450dfc659ba7884a2a9edd1946542dc277f5c30
Jason Willwerscheid <jwillwer@providence.edu>
2025-12-20 14:45:19 -0500

    noisy swimmer updates

--- code/swimmer.R
+++ code/swimmer.R
@@ -1,33 +1,89 @@
-swimmer <- R.matlab::readMat("data/Y.mat")
-dat <- apply(swimmer$Y, 3, as.vector)
-
-dat <- dat - 1
-rows_to_keep <- which(rowSums(dat) > 0)
-dat <- dat[rows_to_keep, ]
-
-t0 <- Sys.time()
-fl <- flashier::flash(dat, ebnm_fn = ebnm_point_exponential, backfit = TRUE)
-tf <- Sys.time() - t0
-
 library(tidyverse)
-fitted_L <- matrix(0, nrow = 32^2, ncol = fl$n_factors)
-fitted_L[rows_to_keep, ] <- ldf(fl, type = "m")$L
+library(flashier)
+source("./code/sim_functions.R")
+source("./code/swimmer_functions.R")
 
-tib <- as_tibble(fitted_L) |>
-  mutate(
-    row = rep(1:32, each = 32),
-    col = rep(1:32, times = 32)
-  ) |>
-  pivot_longer(
-    cols = -c(row, col),
-    names_to = "k",
-    values_to = "loading",
-    names_prefix = "V",
-    names_transform = as.numeric
-  )
-ggplot(tib, aes(x = row, y = col, fill = loading)) +
-  geom_tile() +
-  scale_y_reverse() +
-  scale_fill_gradient(low = "white", high = "black") +
-  facet_wrap(~k) +
-  guides(fill = "none")
+swimmer <- R.matlab::readMat("./data/swimmer.mat")
+dat <- apply(swimmer$Y, 3, as.vector)
+dat <- dat - 1
+
+add_noise <- function(dat, noise_lvl) {
+  shape1 <- ifelse(dat == 0, noise_lvl, 1)
+  shape2 <- ifelse(dat == 0, 1, noise_lvl)
+  noisy_dat <- matrix(rbeta(length(dat), shape1, shape2), nrow = nrow(dat))
+  return(noisy_dat)
+}
+
+# Sample of noisy swimmer images.
+set.seed(1)
+noisy_dat <- add_noise(dat, 0.1)
+R.matlab::writeMat("./data/noisy_swimmer.mat", Y = noisy_dat)
+plot_images(noisy_dat[, sample(ncol(noisy_dat), 6)], nrow = 1, ncol = 6) +
+  theme(strip.text = element_blank())
+ggsave("./output/plots/swimmer_sample.pdf", width = 7, height = 1.5)
+
+# "True" factors.
+parts <- extract_true_swimmer_factors(dat)
+parts <- parts[, c(1, 13, 12,  4, 2, 10, 5, 11,  6,
+                      16,  8, 17, 9, 14, 3,  7, 15)]
+p1 <- plot_images(parts[, 1:9], nrow = 1, ncol = 9) +
+  theme(plot.margin = margin())
+p2 <- plot_images(parts[, 10:17], nrow = 1, ncol = 8) +
+  theme(plot.margin = margin(l = 62))
+cowplot::plot_grid(p1, p2, nrow = 2)
+ggsave("./output/plots/swimmer_true_factors.pdf", width = 8, height = 2)
+
+# EBNMF results.
+fl <- flash(noisy_dat, ebnm_fn = ebnm_point_exponential, greedy_Kmax = 20, backfit = TRUE)
+plot_images(fl$L_pm[, align_cols(fl$L_pm, parts)[1:17]], nrow = 1, ncol = 17)
+
+# Sparse NMF results (DeBruine).
+plotlist <- list()
+for (pen in c(0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5)) {
+  spnmf <- RcppML::nmf(noisy_dat, k = 20, L1 = c(pen, 0), seed = 1)
+  W <- spnmf$w[, spnmf$d > sqrt(.Machine$double.eps), drop = FALSE]
+  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17) +
+    ggtitle(paste("L1 penalty =", pen))
+  plotlist <- c(plotlist, list(p))
+}
+plot_grid(plotlist = plotlist, ncol = 1)
+ggsave("./output/plots/swimmer_debruine.pdf", width = 8, height = 3.5)
+
+setwd("./matlab/")
+Matlab$startServer(matlab = "/Applications/MATLAB_R2025a.app/bin/matlab")
+matlab <- Matlab()
+open(matlab)
+setVerbose(matlab, threshold = 1000000)
+
+# Sparse NMF results (Hoyer).
+plotlist <- list()
+for (pen in c(0.25, 0.5, 0.75, 0.9, 0.95, 0.99)) {
+  spnmf <- run_Hoyer_sparse_nmf(matlab, noisy_dat, k = 20, pen = pen, seed = 1)
+  W <- spnmf$fit$W
+  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17) +
+    ggtitle(paste("Sparseness penalty =", pen))
+  plotlist <- c(plotlist, list(p))
+}
+plot_grid(plotlist = plotlist, ncol = 1)
+ggsave("../output/plots/swimmer_hoyer.pdf", width = 8, height = 5)
+
+# Sparse NMF results (Kim and Park).
+plotlist <- list()
+for (pen in c(0.1, 0.3, 1, 3, 10, 30, 100)) {
+  spnmf <- run_KimPark_sparse_nmf(matlab, noisy_dat, k = 20, L1pen = pen, L2pen = 1, seed = 1)
+  W <- spnmf$fit$W
+  W <- t(t(W) / apply(W, 2, max))
+  if (ncol(W) < 17) {
+    W <- cbind(
+      W, matrix(1e-6 * rexp(nrow(W) * (17 - ncol(W))), nrow = nrow(W), ncol = 17 - ncol(W))
+    )
+  }
+  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17, scale_W = FALSE) +
+    ggtitle(paste("L1 penalty =", pen))
+  plotlist <- c(plotlist, list(p))
+}
+plot_grid(plotlist = plotlist, ncol = 1)
+ggsave("../output/plots/swimmer_kimpark.pdf", width = 8, height = 3.5)
+
+close(matlab)
+setwd("../")
--- code/swimmer_functions.R
+++ code/swimmer_functions.R
@@ -8,7 +8,7 @@
 #   Y <- Y - 1
 #
 extract_true_swimmer_factors <- function (Y) {
-  Y <- Y + 0.0001  
+  Y <- Y + 0.0001
   fit <- flash(Y,ebnm_fn = ebnm_point_exponential,
                backfit = FALSE,var_type = 0)
   L <- ldf(fit)$L
@@ -47,13 +47,16 @@ frobenius_norm_flash <- function (X, fit)
 
 # This takes the m x k matrix of feature weights and plots them as n x
 # n images, where m = n^2. Note that nrow*ncol >= k is expected.
-plot_images <- function (W, n = 32, font_size = 9, nrow = 4, ncol = 4) {
+plot_images <- function (W, n = 32, font_size = 9, nrow = 4, ncol = 4, scale_W = TRUE) {
+  if (scale_W) {
+    W <- t(t(W) / apply(W, 2, max))
+  }
   k <- ncol(W)
   colnames(W) <- paste0("V",1:k)
   dat <- as_tibble(W)
   dat <- mutate(dat,
                 row = rep(1:n,each = n),
-                col = rep(1:n,times = n)) 
+                col = rep(1:n,times = n))
   dat <- pivot_longer(dat,cols = -c(row,col),names_to = "k",
                       values_to = "loading",names_prefix = "V",
                       names_transform = as.numeric)
@@ -61,11 +64,12 @@ plot_images <- function (W, n = 32, font_size = 9, nrow = 4, ncol = 4) {
   return(ggplot(dat,aes(x = row,y = col,fill = loading)) +
     geom_tile() +
     scale_y_reverse() +
-    scale_fill_gradient(low = "white",high = "black") +
+    scale_fill_gradient(low = "white",high = "black", limits = c(0, 1), transform = "sqrt") +
     facet_wrap(~k,nrow = nrow,ncol = ncol) +
     guides(fill = "none",x = "none",y = "none") +
     labs(x = "",y = "") +
     theme_cowplot(font_size = font_size) +
     theme(strip.background = element_blank(),
+          strip.text = element_blank(),
           panel.border = element_rect(color = "black",size = 0.5)))
 }
