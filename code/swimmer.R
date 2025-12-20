13f0277a31df37f925094af0a58e42a04668b7b6
Jason Willwerscheid <jwillwer@providence.edu>
2025-12-20 18:10:10 -0500

    more swimmer updates

--- code/swimmer.R
+++ code/swimmer.R
@@ -26,30 +26,30 @@ ggsave("./output/plots/swimmer_sample.pdf", width = 7, height = 1.5)
 parts <- extract_true_swimmer_factors(dat)
 parts <- parts[, c(1, 13, 12,  4, 2, 10, 5, 11,  6,
                       16,  8, 17, 9, 14, 3,  7, 15)]
-p1 <- plot_images(parts[, 1:9], nrow = 1, ncol = 9) +
-  theme(plot.margin = margin())
-p2 <- plot_images(parts[, 10:17], nrow = 1, ncol = 8) +
-  theme(plot.margin = margin(l = 62))
-cowplot::plot_grid(p1, p2, nrow = 2)
-ggsave("./output/plots/swimmer_true_factors.pdf", width = 8, height = 2)
+plot_images(parts, nrow = 1, ncol = 17)
+ggsave("./output/plots/swimmer_true_factors.pdf", width = 8, height = 0.75)
 
 # EBNMF results.
 fl <- flash(noisy_dat, ebnm_fn = ebnm_point_exponential, greedy_Kmax = 20, backfit = TRUE)
 plot_images(fl$L_pm[, align_cols(fl$L_pm, parts)[1:17]], nrow = 1, ncol = 17) +
-  ggtitle("EBSNMF")
+  ggtitle("EBNMF")
 ggsave("./output/plots/swimmer_flash.pdf", width = 8, height = 0.9)
 
 # Sparse NMF results (DeBruine).
 plotlist <- list()
-for (pen in c(0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5)) {
-  spnmf <- RcppML::nmf(noisy_dat, k = 20, L1 = c(pen, 0), seed = 1)
-  W <- spnmf$w[, spnmf$d > sqrt(.Machine$double.eps), drop = FALSE]
-  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17) +
-    ggtitle(paste("L1 penalty =", pen))
+for (pen in c(0, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6)) {
+  spnmf <- run_RcppML_sparse_nmf(noisy_dat, k = 20, L1pen = pen, seeds = 1:10)
+  W <- spnmf$fit$W
+  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17)
+  if (pen > 0) {
+    p <- p + ggtitle(paste("L1 penalty =", pen))
+  } else {
+    p <- p + ggtitle("No penalty (vanilla NMF)")
+  }
   plotlist <- c(plotlist, list(p))
 }
 plot_grid(plotlist = plotlist, ncol = 1)
-ggsave("./output/plots/swimmer_debruine.pdf", width = 8, height = 6)
+ggsave("./output/plots/swimmer_debruine.pdf", width = 8, height = 8)
 
 setwd("./matlab/")
 Matlab$startServer(matlab = "/Applications/MATLAB_R2025a.app/bin/matlab")
@@ -59,20 +59,24 @@ setVerbose(matlab, threshold = 1000000)
 
 # Sparse NMF results (Hoyer).
 plotlist <- list()
-for (pen in c(0.25, 0.5, 0.75, 0.9, 0.95, 0.99)) {
-  spnmf <- run_Hoyer_sparse_nmf(matlab, noisy_dat, k = 20, pen = pen, seed = 1)
+for (pen in c(0, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99)) {
+  spnmf <- run_Hoyer_sparse_nmf(matlab, noisy_dat, k = 20, pen = pen, seed = 1:10)
   W <- spnmf$fit$W
-  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17) +
-    ggtitle(paste("Sparseness penalty =", pen))
+  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17)
+  if (pen > 0) {
+    p <- p + ggtitle(paste("Sparseness penalty =", pen))
+  } else {
+    p <- p + ggtitle("No penalty (vanilla NMF)")
+  }
   plotlist <- c(plotlist, list(p))
 }
 plot_grid(plotlist = plotlist, ncol = 1)
-ggsave("../output/plots/swimmer_hoyer.pdf", width = 8, height = 5)
+ggsave("../output/plots/swimmer_hoyer.pdf", width = 8, height = 6)
 
 # Sparse NMF results (Kim and Park).
 plotlist <- list()
-for (pen in c(0.1, 0.3, 1, 3, 10, 30, 100)) {
-  spnmf <- run_KimPark_sparse_nmf(matlab, noisy_dat, k = 20, L1pen = pen, L2pen = 1, seed = 1)
+for (pen in c(0, 0.1, 0.3, 1, 3, 10, 30, 100)) {
+  spnmf <- run_KimPark_sparse_nmf(matlab, noisy_dat, k = 20, L1pen = pen, L2pen = 1, seed = 1:10)
   W <- spnmf$fit$W
   W <- t(t(W) / apply(W, 2, max))
   if (ncol(W) < 17) {
@@ -80,12 +84,16 @@ for (pen in c(0.1, 0.3, 1, 3, 10, 30, 100)) {
       W, matrix(1e-6 * rexp(nrow(W) * (17 - ncol(W))), nrow = nrow(W), ncol = 17 - ncol(W))
     )
   }
-  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17, scale_W = FALSE) +
-    ggtitle(paste("L1 penalty =", pen))
+  p <- plot_images(W[, align_cols(W, parts)[1:17]], nrow = 1, ncol = 17, scale_W = FALSE)
+  if (pen > 0) {
+    p <- p + ggtitle(paste("L1 penalty =", pen))
+  } else {
+    p <- p + ggtitle("No penalty (vanilla NMF)")
+  }
   plotlist <- c(plotlist, list(p))
 }
 plot_grid(plotlist = plotlist, ncol = 1)
-ggsave("../output/plots/swimmer_kimpark.pdf", width = 8, height = 6)
+ggsave("../output/plots/swimmer_kimpark.pdf", width = 8, height = 7)
 
 close(matlab)
 setwd("../")
