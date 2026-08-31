014d728d046df3f65d240ec88afca4053b431c83
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-08-31 06:28:29 -0500

    Fixed a typo in compile_montoro_ebmf_outputs.R.

--- code/compile_montoro_ebmf_outputs.R
+++ code/compile_montoro_ebmf_outputs.R
@@ -5,5 +5,5 @@ fl_k12 <- list(timing = res$t,
                fl_ldf = ldf(res$fit,type = "i"))
 fl_k40 <- NULL
 save(list = c("fl_k12","fl_k40"),
-     file = "montoro_ebmf_fits.rds")
-resaveRdaFiles("montoro_ebmf_fits.rds")
+     file = "montoro_ebmf_fits.RData")
+resaveRdaFiles("montoro_ebmf_fits.RData")
