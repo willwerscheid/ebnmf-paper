a19c3d8e0c5d65650278e7aa0cfb70ac48b4fcbb
Peter Carbonetto <peter.carbonetto@gmail.com>
2026-09-03 11:09:48 -0500

    Updated compile_montoro_ebmf_outputs.R to use the k=30 result.

--- code/compile_montoro_ebmf_outputs.R
+++ code/compile_montoro_ebmf_outputs.R
@@ -6,12 +6,12 @@ fl_k12 <- list(timing = res$t,
                L_ghat = res$fit$L_ghat,
                F_ghat = res$fit$F_ghat,
                residuals_sd = res$fit$residuals_sd)
-res <- readRDS("output/montoro-ebmf-40.rds")
-fl_k40 <- list(timing = res$t,
-               fl_ldf = ldf(res$fit,type = "i"),
+res <- readRDS("output/montoro-ebmf.rds")
+fl_k30 <- list(timing = res$t,
+               ldf = ldf(res$fit,type = "i"),
                L_ghat = res$fit$L_ghat,
                F_ghat = res$fit$F_ghat,
                residuals_sd = res$fit$residuals_sd)
-save(list = c("fl_k12","fl_k40"),
+save(list = c("fl_k12","fl_k30"),
      file = "montoro_ebmf_fits.RData")
 resaveRdaFiles("montoro_ebmf_fits.RData")
